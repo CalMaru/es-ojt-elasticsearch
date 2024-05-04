49626fa486ea519be90be465d956ef8edc17e7b5
Yejining <kimyejin.kr@gmail.com>
2024-05-04 11:34:01 +0900

    :sparkles: feat: templates

---
+++ bash/es_template.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+    source .env
+else
+    echo "Error: .env file not found."
+    exit 1
+fi
+
+LOG_PATH=../logs/template
+TEMPLATE_PATH=../data/template
+
+if [ -d "$LOG_PATH" ]; then
+    rm -rf "$LOG_PATH"
+fi
+
+mkdir "$LOG_PATH"
+
+curl -X PUT -u elastic:$ES_PASSWORD "$ES_URL/_scripts/get_all_items" -H "Content-Type: application/json" -d @$TEMPLATE_PATH/get_all_items.json -o $LOG_PATH/get_all_items.log
+echo ""
+curl -X PUT -u elastic:$ES_PASSWORD "$ES_URL/_scripts/autocomplete" -H "Content-Type: application/json" -d @$TEMPLATE_PATH/autocomplete.json -o $LOG_PATH/autocomplete.log
+echo ""
