1b7514beaeb4c869b143b31d49e95ea7fe5a8673
Yejining <kimyejin.kr@gmail.com>
2024-05-07 00:39:18 +0900

    :coffin: feat: es_bulk.sh, es_index.sh에서 byline 관련 부분 삭제

--- bash/es_bulk.sh
+++ bash/es_bulk.sh
@@ -16,13 +16,11 @@ fi
 
 mkdir "$LOG_PATH"
 
-curl -X POST -u elastic:$ES_PASSWORD "$ES_URL/byline/_bulk?pretty" -H "Content-Type: application/x-ndjson" --data-binary @$BULK_PATH/byline.json -o $LOG_PATH/byline.log
-echo ""
 curl -X POST -u elastic:$ES_PASSWORD "$ES_URL/category/_bulk?pretty" -H "Content-Type: application/x-ndjson" --data-binary @$BULK_PATH/category.json -o $LOG_PATH/category.log
-echo ""
-curl -X POST -u elastic:$ES_PASSWORD "$ES_URL/news/_bulk?pretty" -H "Content-Type: application/x-ndjson" --data-binary @$BULK_PATH/news.json -o $LOG_PATH/news.log
 echo ""
 curl -X POST -u elastic:$ES_PASSWORD "$ES_URL/provider/_bulk?pretty" -H "Content-Type: application/x-ndjson" --data-binary @$BULK_PATH/provider.json -o $LOG_PATH/provider.log
 echo ""
 curl -X POST -u elastic:$ES_PASSWORD "$ES_URL/reporter/_bulk?pretty" -H "Content-Type: application/x-ndjson" --data-binary @$BULK_PATH/reporter.json -o $LOG_PATH/reporter.log
+echo ""
+curl -X POST -u elastic:$ES_PASSWORD "$ES_URL/news/_bulk?pretty" -H "Content-Type: application/x-ndjson" --data-binary @$BULK_PATH/news.json -o $LOG_PATH/news.log
 echo ""
