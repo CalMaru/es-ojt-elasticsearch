4f085611038fe7dc2a6e23fd2af3c6bf6461cf67
Yejining <kimyejin.kr@gmail.com>
2024-05-14 14:57:19 +0900

    :memo: feat: es_index.sh 환경변수 수정

--- bash/es_index.sh
+++ bash/es_index.sh
@@ -16,13 +16,13 @@ fi
 
 mkdir "$LOG_PATH"
 
-curl -X PUT "$ES_URL/$CATEGORY" -H "Content-Type: application/json" -d @$INDEX_PATH/$CATEGORY.json -o $LOG_PATH/$CATEGORY.log
+curl -X PUT "$ES_URL/$CATEGORY" -H "Content-Type: application/json" -d @$INDEX_PATH/category.json -o $LOG_PATH/$CATEGORY.log
 echo ""
-curl -X PUT "$ES_URL/$PROVIDER" -H "Content-Type: application/json" -d @$INDEX_PATH/$PROVIDER.json -o $LOG_PATH/$PROVIDER.log
+curl -X PUT "$ES_URL/$PROVIDER" -H "Content-Type: application/json" -d @$INDEX_PATH/provider.json -o $LOG_PATH/$PROVIDER.log
 echo ""
-curl -X PUT "$ES_URL/$REPORTER" -H "Content-Type: application/json" -d @$INDEX_PATH/$REPORTER.json -o $LOG_PATH/$REPORTER.log
+curl -X PUT "$ES_URL/$REPORTER" -H "Content-Type: application/json" -d @$INDEX_PATH/reporter.json -o $LOG_PATH/$REPORTER.log
 echo ""
-curl -X PUT "$ES_URL/$KEYWORD" -H "Content-Type: application/json" -d @$INDEX_PATH/$KEYWORD.json -o $LOG_PATH/$KEYWORD.log
+curl -X PUT "$ES_URL/$KEYWORD" -H "Content-Type: application/json" -d @$INDEX_PATH/keyword.json -o $LOG_PATH/$KEYWORD.log
 echo ""
-curl -X PUT "$ES_URL/$NEWS" -H "Content-Type: application/json" -d @$INDEX_PATH/$NEWS.json -o $LOG_PATH/$NEWS.log
+curl -X PUT "$ES_URL/$NEWS" -H "Content-Type: application/json" -d @$INDEX_PATH/news.json -o $LOG_PATH/$NEWS.log
 echo ""
